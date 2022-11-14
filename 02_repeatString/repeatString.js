2cf064aad1ea97a0a2496155fd5df9e3eee1747d
Garrett Post <Gep100iosgames@gmail.com>
2022-11-13 22:25:00 -0500

    Solve calculator problem

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
-
+function repeatString(string,num) {
+    let repStr = '';
+    if (num>0){
+        for (i=0; i<num; i++){
+            repStr += string;
+        }
+    } else if (num<0) {
+        return 'ERROR';
+    } else {
+        return '';
+    }
+    return repStr
+}
 // Do not edit below this line
 module.exports = repeatString;
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+	let numOut = numOne + numTwo;
+  return numOut;
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+	let numOut = numOne - numTwo;
+  return numOut;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let numOut = array.reduce((a,b) => a + b, 0);
+  return numOut;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let numOut = 1;
+  for (let i = 0; i < array.length; i++){
+    numOut *= array[i];
+  }
+  return numOut;
 };
 
-const power = function() {
-	
+const power = function(num,pow) {
+	let numOut = num ** pow;
+  return numOut;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let numOut = num;
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+      while (num > 1) { 
+        num--; 
+        numOut *= num;
+    }
+    return numOut
+  }
 };
 
 // Do not edit below this line
