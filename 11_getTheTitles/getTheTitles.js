1244eee2a9bf2066048569a81692ae44df5122e6
Garrett Post <Gep100iosgames@gmail.com>
2022-11-14 15:22:54 -0500

    Create function that finds book titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+// loop through the array of objects, take the titles, and push those titles to a new array
+const getTheTitles = function(array) {
+    let newArray=[]
+    for (i=0 ; i < array.length; i++){
+        newArray.push(array[i].title)
+    }
+    return newArray
 };
 
 // Do not edit below this line
