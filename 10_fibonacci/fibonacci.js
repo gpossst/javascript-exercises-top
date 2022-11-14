6b5239d193ff456b75a42203fb8f4176093a1e62
Garrett Post <Gep100iosgames@gmail.com>
2022-11-14 15:13:15 -0500

    Create fibonacci checking function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    let array = [1,1]
+    for (i = 2; i < 50; i++){
+        array.push(array[i-1]+array[i-2])
+    }
+    return array[num-1];
 
 };
 
