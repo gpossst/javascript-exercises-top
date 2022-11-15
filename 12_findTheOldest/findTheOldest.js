6a46199babef839f0dc07c155faeaf5d0133d3ec
Garrett Post <Gep100iosgames@gmail.com>
2022-11-15 11:10:13 -0500

    Create function to find the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+// create an array of the lifetimes of each people, sort the objects by age, return the largest
+const findTheOldest = function(people) {
+    let date = new Date(). getFullYear();
+    people.forEach(person => {
+        if (!person.yearOfDeath){
+            person.age = date - person.yearOfBirth;
+        } else {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+    });
+// finds age
 
-};
+    people.sort((a,b) => {return a.age - b.age});
+//sorts objects by age key
+
+    return people[people.length-1];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
